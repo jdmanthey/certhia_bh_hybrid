7d27811fc4517f84c856f1fd796eff8f518e9740
Joe Manthey <jdmanthey@gmail.com>
2024-10-09 15:25:29 -0500

    Create 00_setup.sh

---
+++ 00_setup.sh
@@ -0,0 +1,17 @@
+# define main working directory
+workdir=/lustre/scratch/jmanthey/05_certhia_hybrids
+
+# make output directories
+cd ${workdir}
+
+mkdir 00_fastq
+mkdir 01_cleaned
+mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_vcf
+mkdir 05_structure
+mkdir 07_window_stats
+mkdir 07_window_stats/windows
+mkdir 10_filter
+mkdir 11_filter_vcf
